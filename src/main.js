ff591dfe1c59f1f47b32306c7bcc61a5bad56f45
justwiner <392110917@qq.com>
2020-04-15 16:53:31 +0800

    feat: Set up routing library at local runtime

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import MessRouter from 'mess-router'
 
+Vue.prototype.$singleSpa = {
+  router: new MessRouter('hash')
+}
+Vue.prototype.$singleSpa = {}
 Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
